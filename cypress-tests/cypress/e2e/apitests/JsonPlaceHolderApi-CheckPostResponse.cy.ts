cb840c2339be8967d32014c7ad64719d50961379
Vin Enguillo <jenguillo@gtreasury.com>
2022-11-07 15:14:48 +0800

    removed unnecessary fixture call

--- cypress-tests/cypress/e2e/apitests/JsonPlaceHolderApi-CheckPostResponse.cy.ts
+++ cypress-tests/cypress/e2e/apitests/JsonPlaceHolderApi-CheckPostResponse.cy.ts
@@ -3,40 +3,36 @@ context('JsonPlaceHolder API', () => {
     describe('Check POST Response from JsonPlaceHolder API', () => {
 
         it('should be able to successfully create a post', () => {
-            cy.fixture('pokeapi-get-pokemon-pikachu.json').then((res) => {
-                cy.request({
-                    method: 'POST',
-                    url: 'https://jsonplaceholder.typicode.com/posts',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: {
-                        userId : 1212,
-                        title: "Vin's First Post",
-                        body: "Hi! This is my first post."
-                    }
-                }).its('status').should('eq', 201);
-            });
+            cy.request({
+                method: 'POST',
+                url: 'https://jsonplaceholder.typicode.com/posts',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    userId: 1212,
+                    title: "Vin's First Post",
+                    body: "Hi! This is my first post."
+                }
+            }).its('status').should('eq', 201);
         });
 
         it('should have a response with the correct values supplied in the request', () => {
-            cy.fixture('pokeapi-get-pokemon-pikachu.json').then((res) => {
-                cy.request({
-                    method: 'POST',
-                    url: 'https://jsonplaceholder.typicode.com/posts',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: {
-                        userId : 1212,
-                        title: "Vin's First Post",
-                        body: "Hi! This is my first post."
-                    }
-                }).then(({body}) => {
-                    expect(body.userId).to.eq(1212);
-                    expect(body.title).to.eq("Vin's First Post");
-                    expect(body.body).to.eq("Hi! This is my first post.");
-                });
+            cy.request({
+                method: 'POST',
+                url: 'https://jsonplaceholder.typicode.com/posts',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    userId: 1212,
+                    title: "Vin's First Post",
+                    body: "Hi! This is my first post."
+                }
+            }).then(({ body }) => {
+                expect(body.userId).to.eq(1212);
+                expect(body.title).to.eq("Vin's First Post");
+                expect(body.body).to.eq("Hi! This is my first post.");
             });
         });
     })
